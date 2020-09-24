73d98818a395945dd5e52eae8e489389837b5f97
David <david00gbg@hotmail.com>
2020-09-24 14:20:42 +0200

    Added methods for GameState interface

--- src/main/java/model/insanityHauntState.java
+++ src/main/java/model/insanityHauntState.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.List;
+
 public class insanityHauntState implements GameState {
+    @Override
+    public void init(Game game, List<Player> playerList, Player activePlayer) {
+
+    }
+
+    @Override
+    public void turn(Player activePlayer, Event event) {
+
+    }
 }
--- src/main/java/model/preHauntState.java
+++ src/main/java/model/preHauntState.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.List;
+
 public class preHauntState implements GameState{
+    @Override
+    public void init(Game game, List<Player> playerList, Player activePlayer) {
+        
+    }
+
+    @Override
+    public void turn(Player activePlayer, Event event) {
+
+    }
 }
