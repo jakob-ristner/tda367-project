dc6d8f3b9ec0e886f925d90bd9156b922e61efb5
David <david00gbg@hotmail.com>
2020-09-24 13:54:53 +0200

    Added methods for GameState interface

--- src/main/java/model/GameState.java
+++ src/main/java/model/GameState.java
@@ -1,4 +1,10 @@
 package model;
 
+import java.util.List;
+
 public interface GameState {
+     void init(Game game, List<Player> playerList,Player activePlayer);
+     void turn(Player activePlayer, Event event);
+
+
 }
