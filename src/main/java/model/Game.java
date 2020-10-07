6ad4cbd57f99ad8ee03ddeafa9f7511d3570a389
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2020-10-07 15:37:25 +0200

    EB, KG, worked init method in insanityHauntState for setting up rules

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -261,4 +261,8 @@ public class Game implements ControllerObservable{
     public List<String> getCurrentPlayerStatsAsStrings() {
         return getCurrentPlayer().getCharacterStatsAsStrings();
     }
+
+    public Board getBoard(){
+        return board;
+    }
 }
