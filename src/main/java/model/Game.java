40fe5d902b77b8b92a7a9459773735e86a2803c7
rille2 <jakob.o.ristner@gmail.com>
2020-10-21 11:35:08 +0200

    added meeting protocol

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -361,7 +361,7 @@ public class Game implements ControllerObservable {
     }
 
     private void checkForHauntInit(){
-        if(eventCounter == 1 && gameState == null){
+        if(eventCounter == 10217239 && gameState == null){
             gameState = getRandomHaunt();
             initHaunt();
             notifyHaunt();
